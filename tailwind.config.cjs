e8cdb65aaf15c29699c6ac6542cd68ed58caa57e
MukulWaval <mukulwaval2000@gmail.com>
2024-11-25 10:32:14 +0530

    Hide scrollbar

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,18 +10,47 @@ module.exports = {
         tertiary: "#151030",
         "black-100": "#100d25",
         "black-200": "#090325",
-        "white-100": "#f3f3f3",
+        "white-100": "#f3f3f3"
       },
       boxShadow: {
-        card: "0px 35px 120px -15px #211e35",
+        card: "0px 35px 120px -15px #211e35"
       },
       screens: {
-        xs: "450px",
+        xs: "450px"
       },
       backgroundImage: {
-        "hero-pattern": "url('/src/assets/herobg.png')",
-      },
-    },
+        "hero-pattern": "url('/src/assets/herobg.png')"
+      }
+    }
   },
-  plugins: [],
+  plugins: [
+    function ({ addBase }) {
+      addBase({
+        /* Apply to all elements globally */
+        "*": {
+          scrollbarWidth: "none", // Hides scrollbar in non-Webkit browsers
+          scrollbarColor: "transparent transparent" // Transparent scrollbar thumb and track
+        },
+        /* Webkit-based styling for hiding the scrollbar */
+        "&::-webkit-scrollbar": {
+          width: "0px", // Hide scrollbar in Webkit browsers
+          height: "0px" // Hide horizontal scrollbar
+        },
+        "&::-webkit-scrollbar-thumb": {
+          backgroundColor: "transparent", // Make the thumb transparent
+          borderRadius: "0px" // Remove rounding for the thumb
+        },
+        "&::-webkit-scrollbar-track": {
+          backgroundColor: "transparent" // Make the track transparent
+        },
+        "&::-webkit-scrollbar-button": {
+          display: "none" // Hide the scrollbar buttons (arrows)
+        },
+        /* Allow scrolling functionality */
+        "html, body": {
+          overflow: "auto" // Ensure scrolling is still possible
+        }
+      });
+    }
+  ]
 };
